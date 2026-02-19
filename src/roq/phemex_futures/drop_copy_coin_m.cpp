077b2cf89d2a21753ed33752e8f38ab854441633
thraneh <thraneh@gmail.com>
2026-02-19 10:06:45 +0100

    prepare more dynamic WS URI query params

--- src/roq/phemex_futures/drop_copy_coin_m.cpp
+++ src/roq/phemex_futures/drop_copy_coin_m.cpp
@@ -60,7 +60,6 @@ auto create_connection(auto &handler, auto &settings, auto &context) {
       // proxy
       .proxy = {},
       // http
-      .query = {},
       .user_agent = ROQ_PACKAGE_NAME,
       .request_timeout = {},
       .ping_frequency = settings.ws.ping_freq,
