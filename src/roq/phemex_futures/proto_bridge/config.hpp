ea6281b29c4a9eda801d8ccc7bad55cd3b97406d
thraneh <thraneh@gmail.com>
2026-07-04 03:18:58 +0000

    preparing roq-*-proto-bridge

---
+++ src/roq/phemex_futures/proto_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/phemex_futures/gateway/config.hpp"
+
+namespace roq {
+namespace phemex_futures {
+namespace proto_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace proto_bridge
+}  // namespace phemex_futures
+}  // namespace roq
